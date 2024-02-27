172eb4f2e292beb8f3d43c4c138e7af990af98dd
Mantas <mantasjurkuvenas@gmail.com>
2024-02-27 09:47:09 +0200

    10 current  2024-02-27 09:47:07  23.11.4761.5bf1cadb72ab  6.1.79                          *

--- nixbuild.sh
+++ nixbuild.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
   cp -r /etc/nixos ~/dotfiles/
+  sudo nixos-rebuild switch
   current=$(nixos-rebuild list-generations | grep current)
   git add .
   git commit -am "$current"
