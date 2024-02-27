6006f889593b1c0302641067df3e13088da2a84f
Mantas <mantasjurkuvenas@gmail.com>
2024-02-27 09:44:43 +0200

    9 current   2024-02-27 09:36:39  23.11.4761.5bf1cadb72ab  6.1.79                          *

---
+++ nixbuild.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+  cp -r /etc/nixos ~/dotfiles/
+  current=$(nixos-rebuild list-generations | grep current)
+  git add .
+  git commit -am "$current"
+  notify-desktop "Nix OS Rebuilt OK"
+
+
