802582ccde6f61c312390ac1c68e0aea652c4b45
Mantas <mantasjurkuvenas@gmail.com>
2024-03-05 11:27:04 +0200

    sync dotfiles

--- fish/config.fish
+++ fish/config.fish
@@ -2,14 +2,17 @@ if status is-interactive
 starship init fish | source
 zoxide init fish | source
 
+function fish_greeting
+    echo Today is (set_color yellow; date; set_color normal) running  $hostname
+end
+
+
 function weather
     curl curl v2d.wttr.in
 end
 
-
 eval (zellij setup --generate-auto-start fish | string collect)
     # Commands to run in interactive sessions can go here
-    #a
     alias vi="nvim"
     alias cd="z"
 end
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,22 +1,20 @@
 local wezterm = require("wezterm")
--- The filled in variant of the < symbol
 return {
 	-- option	= value	, [default] comment
 
 	-- Fonts
-	font = wezterm.font 'FiraCode Nerd Font Mono', -- [JetBrains Mono]
+	font = wezterm.font("FiraCode Nerd Font Mono"), -- [JetBrains Mono]
 	-- font  	= wezterm.font("JetBrains Mono", {weight="Bold",italic=true})	, -- [JetBrains Mono] Name with parameters
-	font_size = 11.0,              -- [12.0]
+	font_size = 11.0, -- [12.0]
 
 	-- Colors
-	color_scheme = "Gruvbox Dark", -- full list @ wezfurlong.org/wezterm/colorschemes/index.html
-
+	color_scheme = "Gruvbox Material (Gogh)",
+	--color_scheme = 'Gruvbox Dark (Gogh)',
+	--
 	-- Appearance
-	window_background_opacity = 0.8,  -- [1.0] alpha channel value with floating point numbers in the range 0.0 (meaning completely translucent/transparent) through to 1.0 (meaning completely opaque)
-	enable_tab_bar = true,            -- [true]
+	window_background_opacity = 0.7,
 	hide_tab_bar_if_only_one_tab = true, -- [false] hide the tab bar when there is only a single tab in the windowa
 	default_prog = { "fish" },
-
 	window_frame = {
 		font = wezterm.font({ family = "Roboto", weight = "Bold" }),
 		-- Default to 10. on Windows but 12.0 on other systems
